ba2dc53352fba4b552477758b000353bba7daaf8
ViktorStevanovic <stevanovic.lavoro@gmail.com>
2024-04-05 16:01:32 +0200

    chenged time-zone to match the Europe/Rome time in database

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\CategoryController;
 use App\Http\Controllers\Api\ExpenseController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -8,3 +9,4 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 Route::apiResource('/expenses', ExpenseController::class);
+Route::apiResource('/categories', CategoryController::class);
